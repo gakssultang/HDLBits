4481ebcaf709de083355b62a1aee87f9f0ee23e8
gakssultang <mirror1236@gmail.com>
2023-12-13 17:54:07 +0900

    commit

---
+++ Circuits/Combinational Logic/adder3.v
@@ -0,0 +1,18 @@
+module full_adder( 
+    input a, b, cin,
+    output cout, sum );
+
+    assign sum = a ^ b ^ cin;
+    assign cout = a&cin | b&cin | a&b;
+
+endmodule
+
+module top_module( 
+    input [2:0] a, b,
+    input cin,
+    output [2:0] cout,
+    output [2:0] sum );
+
+    full_adder FA1[2:0] (a[2:0], b[2:0], {cout[1:0], cin}, cout[2:0], sum[2:0]);
+
+endmodule
