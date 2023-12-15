b973a1b2d9ed5812f380381202ddc5367ab1248f
gakssultang <mirror1236@gmail.com>
2023-12-16 03:44:15 +0900

    2023dec16

--- Circuits/Combinational Logic/kmap4.v
+++ Circuits/Combinational Logic/kmap4.v
@@ -8,7 +8,17 @@ module top_module(
     /* Lazy Solution */
     assign out = ({a,b,c,d} == 4'b1000 || {a,b,c,d} == 4'b0100 || {a,b,c,d} == 4'b0010 || {a,b,c,d} == 4'b0001 || {a,b,c,d} == 4'b0111 || {a,b,c,d} == 4'b1011 || {a,b,c,d} == 4'b1101 || {a,b,c,d} == 4'b1110) ? 1'b1 : 1'b0;
     
-    /* After I recognized this is k-map of xor4 */
+    /* Simplify Lazy Solution */
+    /*
+    always @(*) begin
+        case({a,b,c,d})
+            4'b1000, 4'b0100, 4'b0010, 4'b0001, 4'b0111, 4'b1011, 4'b1101, 4'b1110: out = 1'b1;
+            default: out = 1'b0;
+        endcase
+    end
+    */
+
+    /* After I recognized that this is k-map of xor4 */
     /*
     assign out = (a ^ b ^ c ^ d);
     */
