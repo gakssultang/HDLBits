99e3e4ba50ec3f5f3ab9b50db814a629f9503b38
gakssultang <mirror1236@gmail.com>
2023-12-16 02:30:50 +0900

    2023dec16

---
+++ Circuits/Combinational Logic/kmap2.v
@@ -0,0 +1,16 @@
+
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  );
+
+	/* 0s are less then 1s, so I just grouped 0s and inverted them. */
+    assign out = ~( (a&b&!c) | (b&!c&d) | (!a&!b&c&d) | (a&c&!d) );
+    /* you can try this */
+    /*
+    assign out = (!a&!d) | (!b&!c) | (b&c&d) | (a&c&d);
+    */
+    
+endmodule
